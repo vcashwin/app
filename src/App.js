93bb1355b6a6af8d7236c98f539c539e48e1e03c
Ashwin V C <ashwinvc@microsoft.com>
2023-01-24 19:11:16 +0530

    fix avatar issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Avatar } from "@fluentui/react-components";
 import { TbSun, TbMoonStars } from "react-icons/tb";
 import * as React from "react";
 
@@ -23,13 +22,10 @@ function App() {
   return (
     <div className="flex flex-col lg:flex-row justify-center items-center bg-gray-200 font-sohne min-h-screen p-6 dark:bg-[#1A1B1E] dark:text-[#C1C2C5]">
       <div className="flex flex-col lg:flex-row items-center justify-center space-x-4">
-        <Avatar
-          name="Ashwin V C"
-          image={{
-            src: "https://pbs.twimg.com/profile_images/1617809099533742080/-NdBFT7q_400x400.jpg",
-          }}
-          size={128}
-          className="mb-4 lg:mr-4 lg:mb-0"
+        <img
+          src="https://pbs.twimg.com/profile_images/1617809099533742080/-NdBFT7q_400x400.jpg"
+          className="mb-4 lg:mr-4 lg:mb-0 w-32 h-32 rounded-full object-cover"
+          alt="Ashwin V C"
         />
         <div className="flex-1 max-w-xl text-center lg:text-left">
           <div className="flex items-center font-extrabold text-3xl mb-4 justify-center lg:justify-start">
