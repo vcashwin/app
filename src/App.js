075f20d82ab313f0bbbb404e0e308a3250b512fe
Ashwin V C <ashwinvc@microsoft.com>
2023-01-24 18:36:59 +0530

    first cut

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Avatar } from "@fluentui/react-components";
+import { TbSun, TbMoonStars } from "react-icons/tb";
+import * as React from "react";
+
+function DarkModeButton() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+  const handleClick = () => {
+    document.documentElement.classList.toggle("dark");
+    setIsDarkMode((dm) => !dm);
+  };
+  return (
+    <div className="cursor-pointer mt-1 p-2 rounded-lg bg-[#F8F9FA] dark:bg-[#1F2225]">
+      {isDarkMode ? (
+        <TbSun className="w-6 h-6 stroke-yellow-500" onClick={handleClick} />
+      ) : (
+        <TbMoonStars className="w-6 h-6 stroke-sky-500" onClick={handleClick} />
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col lg:flex-row justify-center items-center bg-gray-200 font-sohne min-h-screen p-6 dark:bg-[#1A1B1E] dark:text-[#C1C2C5]">
+      <div className="flex flex-col lg:flex-row items-center justify-center space-x-4">
+        <Avatar
+          name="Ashwin V C"
+          image={{
+            src: "https://pbs.twimg.com/profile_images/1617809099533742080/-NdBFT7q_400x400.jpg",
+          }}
+          size={128}
+          className="mb-4 lg:mr-4 lg:mb-0"
+        />
+        <div className="flex-1 max-w-xl text-center lg:text-left">
+          <div className="flex items-center font-extrabold text-3xl mb-4 justify-center lg:justify-start">
+            <p className="mr-4">I am, Ashwin V C</p>
+            <DarkModeButton />
+          </div>
+          <p className="mb-6">
+            A software engineer at Microsoft, where I lead the development of
+            modern web applications using React, Tailwind and other latest tech.
+            I am an enthusiast about Tech and Finance, tinkering with my ideas
+            whilst striving to achieve greatness in these fields.
+          </p>
+          <p className="">
+            Currently building
+            <span className="font-sohne-bold text-md ml-1">
+              Assemble Growth Inc.
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={teamsLightTheme}>
+      <App />
+    </FluentProvider>
   </React.StrictMode>
 );
 
